7368597e116b2f101e73416abfca517b76325160
catalin.giurgiu <catalin.giurgiu@softvision.com>
2021-04-06 14:10:15 +0300

    added controller

---
+++ my_beer_service/src/main/java/guru/framework/my_beer_service/web/controller/BeerController.java
@@ -0,0 +1,33 @@
+package guru.framework.my_beer_service.web.controller;
+
+import guru.framework.my_beer_service.web.model.BeerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId")UUID beerId){
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDTO> saveBeer(@RequestBody BeerDTO beerDTO){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beerDTO){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> deleteBeerById(@PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
