64f15ffa7ad270a68621f1a2238d2b493f2177a0
catalin.giurgiu <catalin.giurgiu@softvision.com>
2021-04-06 14:00:22 +0300

    added web data model

---
+++ my_beer_service/src/main/java/guru/framework/my_beer_service/web/model/BeerDTO.java
@@ -0,0 +1,29 @@
+package guru.framework.my_beer_service.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDTO {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+    private BeerStyle beerStyle;
+    private Long upc;
+
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ my_beer_service/src/main/java/guru/framework/my_beer_service/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package guru.framework.my_beer_service.web.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, PALE_ALE, SAISON
+}
