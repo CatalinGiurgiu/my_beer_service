09026c8fcfe44cfb75e20d805440a66927946b8a
catalin.giurgiu <catalin.giurgiu@softvision.com>
2021-04-06 13:44:42 +0300

    Initial commit with the Spring dependencies added

---
+++ my_beer_service/src/main/java/guru/framework/my_beer_service/MyBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.framework.my_beer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBeerServiceApplication.class, args);
+	}
+
+}
---
+++ my_beer_service/src/test/java/guru/framework/my_beer_service/MyBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.my_beer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
